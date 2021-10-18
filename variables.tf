cec90d3cace665885bbdfbe98949c9f6c334297c
rohan4linux <39763276+rohan4linux@users.noreply.github.com>
2021-10-18 11:39:06 +0530

    added variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "AKIAQQ4UKHAXU5KZTFUS"
+}
+
+variable "secret_key" {
+  default = "mW0c6VqD1hY9FI/HbH74bL6VdUYdGTClvl/f5xDD"
+}
+
+variable "app_count" {
+  type = number
+  default = 1
+}
